e4d842e31b7b61342a303e21bc8083bdf60effc7
conradskittles <conradszklarz@gmail.com>
2018-06-19 14:50:22 -0500

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
+  def index
+    response = Unirest.get("http://localhost:3000/users")
+    @users = response.body
+    render 'index.html.erb'
+  end
+
   def new
     render 'new.html.erb'
   end
 
   def create
     client_params = {
-                      name: params[:name],
                       email: params[:email],
                       password: params[:password],
                       password_confirmation: params[:password_confirmation]
                       }
 
     response = Unirest.post(
-                            "http://localhost:3000/api/users",
+                            "http://localhost:3000/users",
                             parameters: client_params
                             )
     
@@ -22,7 +27,55 @@ class UsersController < ApplicationController
       redirect_to '/'
     else
       flash[:warning] = 'Invalid email or password!'
-       redirect_to '/signup'
+      redirect_to '/signup'
+    end
+  end
+
+  def edit
+    response = Unirest.get("http://localhost:3000/users/#{params[:id]}")
+    @user = response.body
+    render 'edit.html.erb'
+  end
+
+  def show
+    user_id = params[:id]
+    response = Unirest.get("http://localhost:3000/users/#{user_id}")
+    @user = response.body
+    render 'show.html.erb'
+  end
+
+  def update
+    @user = {
+                      'id' => params[:id],
+                     'email' => params[:email],
+                     
+                    }
+
+    response = Unirest.patch(
+                            "http://localhost:3000/users/#{params[:id]}",
+                            parameters: @user
+                            )
+
+    if response.code == 200
+      flash[:success] = "Successfully updated Profile"
+      redirect_to "/users/#{params[:id]}"
+    elsif response.code == 401
+      flash[:warning] = "You are not authorized to update this profile"
+      redirect_to '/'
+    else
+      @errors = response.body['errors']
+      render 'edit.html.erb'
+    end
+  end
+
+  def destroy
+    response = Unirest.delete("http://localhost:3000/users/#{params['id']}")
+    if response.code == 200
+    flash[:success] = "Successfully removed profile"
+    redirect_to "/"
+    else
+      flash[:warning] = "You are not authorized"
+      redirect_to '/'
     end
   end
 end
